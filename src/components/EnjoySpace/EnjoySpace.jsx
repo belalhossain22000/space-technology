94a839dd7d9518728a88becb52fe035287f892d5
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-08-23 01:40:12 +0600

    aded modify space section

--- src/components/EnjoySpace/EnjoySpace.jsx
+++ src/components/EnjoySpace/EnjoySpace.jsx
@@ -7,19 +7,19 @@ import Reveals from '@/utils/Reveals/Reveals';
 const planetData = [
     {
         id: 1,
-        image: "/moon.png",
+        image: "/moon3.png",
         name: "moon",
         title: "Dune Buggy Tours"
     },
     {
         id: 2,
-        image: "/saturn.png",
+        image: "/stun3.png",
         name: "Saturn",
         title: "Canyon Adventures"
     },
     {
         id: 3,
-        image: "/jupitar.png",
+        image: "/jupiar3.png",
         name: "Jupiter",
         title: "Ocean Aerial Tours"
     },
@@ -28,31 +28,31 @@ const planetData = [
 const EnjoySpace = () => {
     return (
         <section className=' text-white w-[1170px] max-w-full mx-auto '>
-            <div className='p-5 lg:p-0 flex justify-center items-center'>
+            <div className='p-5 lg:p-0 flex justify-center items-center m-0'>
                 <Reveals>
-                    <h2 style={{ fontFamily: 'Bebas Neue' }} className='md:text-[55px]  text-[40px] text-center font-normal leading-[60px] tracking-[0.55px] lg:pt-[46px] xl:mb-[132px]'>Enjoying Space Like Never Before</h2>
+                    <h2 style={{ fontFamily: 'Bebas Neue' }} className='md:text-[55px]  text-[40px] text-center font-normal leading-[60px] tracking-[0.55px]  '>Enjoying Space Like Never Before</h2>
                 </Reveals>
             </div>
-            <div className='flex flex-wrap items-center justify-center gap-[23px] xl:justify-between'>
+            <div className='flex flex-wrap items-center justify-center gap-[23px] xl:justify-between '>
                 {/* card */}
                 {
                     planetData.map(({ title, name, image, id }) => <motion.div
-                        whileHover={{ scale: 1.1, background: 'linear-gradient(180deg, rgba(255, 7, 114, 0.00) 14.62%, #FF5A09 100%)', }}
+                        whileHover={{ scale: 1.2, background: 'linear-gradient(180deg, rgba(255, 7, 114, 0.00) 14.62%, #FF5A09 100%)', }}
                         whileTap={{ scale: 0.9 }}
 
-                        key={id} className='h-[408px] w-[80%] md:w-[352px] text-center rounded-[19px] mt-[50px]' style={{ background: "linear-gradient(180deg, rgba(38, 1, 83, 0.00) 14.62%, #250153 100%)" }} >
+                        key={id} className='h-[408px] w-[352px] text-center rounded-[19px] mt-[50px]' style={{ background: "linear-gradient(180deg, rgba(38, 1, 83, 0.00) 14.62%, #250153 100%)" }} >
                         {/* image */}
-                        <div className='flex justify-center'>
+                        <div className='flex justify-center mb-[12px]'>
                             <img className='h-[250px] w-[250px]' src={image} alt="" />
                         </div>
                         {/* text */}
-                        <div className='px-5 flex justify-center items-center flex-col' style={{ fontFamily: 'Roboto' }}>
+                        <div className='px-5 flex justify-center items-center flex-col ' style={{ fontFamily: 'Roboto' }}>
                             <Reveals>
                                 <p className='text-[#FFF] text-[29px] font-medium '>{name}</p>
                             </Reveals>
                             <div className='w-[40px] h-[2px] mx-auto' style={{ background: "linear-gradient(141deg, #FFB018 0%, #FF2900 63.83%)" }}></div>
                             <Reveals>
-                                <h4 className='text-[34px] font-medium'>{title}</h4>
+                                <h4 className='sm:text-[34px] text-[30px] text font-medium'>{title}</h4>
                             </Reveals>
                         </div>
                     </motion.div>)
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,8 +13,7 @@ const Hero = () => {
     return (
         <section className='lsm:bg-[url("/heroimage.png")] bg-no-repeat bg-center  bg-cover xl:bg-auto'>
             <div className='xl:w-[1170px] mx-auto max-w-full xl:px-0'>
-                <div className='lg:pb-[154px] md:pb-[75px] pb-[55px] lg:pt-[236px] pt-[110px] md:pt-[120px]' >
-                {/* <Image className='w-full h-auto absolute top-0 z-10' src={heroImage1}/> */}
+                <div className='lg:pb-[154px] md:pb-[75px] pb-[55px] lg:pt-[236px] pt-[110px] md:pt-[120px]'>
                     <Reveals>
                         <h1 style={{ fontFamily: 'Bebas Neue' }} className=' w-fit text-[#FFF] text-[66px] leading-[70px] lg:text-[100px] font-normal lg:leading-[100px] tracking-[(-0.5px)] px-5 relative  z-20'> PUT YOUR FOOTPRINT IN<br />
                             <span className='' style={{
@@ -26,8 +25,9 @@ const Hero = () => {
                             }}>SPACE TRAVEL</span>
                         </h1>
                     </Reveals>
+                    <Image className='w-full h-auto lsm:hidden' src={heroImage} alt="Hero Image" />
                     <div className='px-5 relative z-20'>
-                        <motion.button whileHover={{ background: 'none', transition: { duration: 0.8 } }} className='text-white text-lg lsm:text-[24px] font-medium flex items-center justify-between lg:gap-[67px] gap-[8px] md:gap-[20px]  md:py-[25px] py-[11px] px-[17px] md:px-[33px] border-[3px] border-white  rounded-[133.962px] lg:mt-[50px] md:mt-[30px] mt-[20px] transition-all duration-1000  w-full h-[60px] lsm:w-fit md:h-fit' style={{ fontFamily: 'Roboto', background: "var(--p-2, linear-gradient(141deg, #FFB018 0%, #FF2900 63.83%))" }}>Get Started <BsArrowRight className='text-[24px] font-medium' /></motion.button>
+                        <motion.button whileHover={{ background: 'none', transition: { duration: 0.8 } }} className='text-white text- lsm:text-[24px] font-medium flex items-center justify-between lg:gap-[67px] gap-[8px] md:gap-[20px]  md:py-[25px] py-[11px] px-[17px] md:px-[33px] border-[3px] border-white  rounded-[133.962px] lg:mt-[50px] md:mt-[30px] mt-[20px] transition-all duration-1000  w-full h-[60px] lsm:w-fit md:h-fit' style={{ fontFamily: 'Roboto', background: "var(--p-2, linear-gradient(141deg, #FFB018 0%, #FF2900 63.83%))" }}>Get Started <BsArrowRight className='text-[24px] font-medium' /></motion.button>
                     </div>
                 </div>
             </div>
